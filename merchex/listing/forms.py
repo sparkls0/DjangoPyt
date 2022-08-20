55b7b8db0d5f71df93c9930d184c4e2a1b031f59
salfysox ws <salfysoxws@salfysoxs-MacBook-Pro.local>
2022-08-21 00:05:10 +0200

    button animation hover/active css

--- merchex/listing/forms.py
+++ merchex/listing/forms.py
@@ -6,7 +6,7 @@ class ContactUsForm(forms.Form):
     name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'contact-form-input'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'contact-form-input'}))
     phone = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'contact-form-input'}))
-    message = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'class': 'contact-form-message'}))
+    message = forms.CharField(max_length=1000, widget=forms.Textarea(attrs={'class': 'contact-form-message', 'rows': 3 }))
     
     
     
