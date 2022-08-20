46db83f0e475770f3fbee810295bac3b04519bfd
salfysox ws <salfysoxws@salfysoxs-MacBook-Pro.local>
2022-08-20 23:39:39 +0200

    css change on the contact form

--- merchex/listing/forms.py
+++ merchex/listing/forms.py
@@ -3,10 +3,10 @@ from django import forms
 
 class ContactUsForm(forms.Form):
     
-    name = forms.CharField(required=False)
-    email = forms.EmailField()
-    phone = forms.CharField(required=False)
-    message = forms.CharField(max_length=1000)
+    name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'contact-form-input'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'contact-form-input'}))
+    phone = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'contact-form-input'}))
+    message = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'class': 'contact-form-message'}))
     
     
     
